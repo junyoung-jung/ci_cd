08804e5ff97c0957ee66db8661b728333017e1f0
Jimmy <junyoung.jung@susimdal.com>
2023-12-04 15:31:41 +0900

    Initial Commit

---
+++ ci_cd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ci_cd
+//
+//  Created by 정준영 on 12/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
